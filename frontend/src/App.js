bcbbaa97ce01be4b46e73de01f2e8859bcf4015a
Tasnim Bushra <tasnim.bushra@g.bracu.ac.bd>
2022-08-18 02:43:32 +0600

    images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
+import data from './data';
+
 function App() {
   return (
-    <div className="App">
+    <div>
       <header>
-        <a href="/">tickersee</a>
+        <a href="/">tickersee </a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <img src={product.image} alt={product.name} />
+              <p>{product.name}</p>
+              <p>{product.price}</p>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
